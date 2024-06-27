a37573425fc4d903c35b8b0f3e9321019fdf0258
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2024-06-27 19:59:32 +0000

    feat(cli): validate resource;
    - Added validate resource command.

--- ckanext/validation/cli.py
+++ ckanext/validation/cli.py
@@ -3,6 +3,10 @@ import click
 
 from ckanext.validation.model import create_tables, tables_exist
 
+# (canada fork only): add run_validation command
+from ckan.plugins.toolkit import get_action, ObjectNotFound
+from ckanext.validation.jobs import run_validation_job
+
 
 @click.group()
 def validation():
@@ -19,3 +23,16 @@ def init_db():
 
     create_tables()
     print(u"Validation tables created")
+
+
+# (canada fork only): add run_validation command
+@validation.command()
+@click.option('-r', '--resource-id', type=click.STRING, help='A CKAN Resource ID.', required=True)
+def run_validation(resource_id):
+    """Runs validation instantly for a given resource."""
+    try:
+        resource = get_action('resource_show')({"ignore_auth": True}, {"id": resource_id})
+    except ObjectNotFound:
+        click.echo("Resource not found: %s" % resource_id)
+        click.Abort
+    run_validation_job(resource)
