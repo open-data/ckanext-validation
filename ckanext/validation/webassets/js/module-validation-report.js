f3065b69f36beccbfdd53d1fc8c8a2cc9e9a0fe6
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2024-07-09 13:32:53 +0000

    fix(js): report jsonify;
    - JSON report for frictionless JS API.

--- ckanext/validation/webassets/js/module-validation-report.js
+++ ckanext/validation/webassets/js/module-validation-report.js
@@ -8,6 +8,10 @@ ckan.module('validation-report', function (jQuery) {
     initialize: function() {
       let element = document.getElementById('report')
       let report = this.options.report
+      // (canada fork only): parse report into object
+      if( typeof report != 'undefined' && report.length > 0 ){
+        report = JSON.parse(report)
+      }
       frictionlessComponents.render(frictionlessComponents.Report, { report }, element)
     }
   }
