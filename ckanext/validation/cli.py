9aefb19ec3c4df72afda57d9ccf481c84c8c0bc6
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2024-11-19 14:37:50 +0000

    feat(cli): improved run validation command;
    - Added sync mode capability to `run-validation` command.
    - Use action instead of calling method directly.

--- ckanext/validation/cli.py
+++ ckanext/validation/cli.py
@@ -5,7 +5,6 @@ from ckanext.validation.model import create_tables, tables_exist
 
 # (canada fork only): add run_validation command
 from ckan.plugins.toolkit import get_action, ObjectNotFound
-from ckanext.validation.jobs import run_validation_job
 
 
 @click.group()
@@ -29,8 +28,9 @@ def init_db():
 @validation.command()
 @click.option('-r', '--resource-id', type=click.STRING, help='A CKAN Resource ID.', required=False)
 @click.option('-d', '--dataset-id', type=click.STRING, help='A CKAN Dataset ID.', required=False)
-@click.option('-s', '--skip-xloader', type=click.BOOL, help='Skip XLoadering the resource after validation.', is_flag=True)
-def run_validation(resource_id=None, dataset_id=None, skip_xloader=False):
+@click.option('-s', '--skip-xloader', type=click.BOOL, help='Skip XLoadering the resource after validation.', is_flag=True, default=False)
+@click.option('-S', '--sync', type=click.BOOL, help='Run the validation job in Sync mode (right away in the CLI).', is_flag=True, default=False)
+def run_validation(resource_id=None, dataset_id=None, skip_xloader=False, sync=False):
     """Runs validation instantly for a given dataset or resource."""
     if resource_id and dataset_id:
         click.echo("--resource-id and --dataset-id are mutually exclusive")
@@ -51,5 +51,7 @@ def run_validation(resource_id=None, dataset_id=None, skip_xloader=False):
             click.echo("Dataset not found: %s" % resource_id)
             click.Abort
     for resource in resources:
-        run_validation_job(resource, skip_xloader)
+        get_action('resource_validation_run')({"ignore_auth": True}, {"resource_id": resource.get('id'),
+                                                                      "async": False if sync else True,
+                                                                      "skip_xloader": skip_xloader})
     click.echo("\nDONE!")
