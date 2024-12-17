61cc99a59932bcb090e38c201fed1f1375ea6d36
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2024-12-17 19:31:25 +0000

    fix(verb): verbiage;
    - `White list` -> `allowed domains`.

--- ckanext/validation/logic.py
+++ ckanext/validation/logic.py
@@ -4,7 +4,7 @@ import datetime
 import logging
 import json
 
-# (canada fork only): upload and white listed sources
+# (canada fork only): upload and allowed domain sources
 from six.moves.urllib.parse import urlsplit
 
 # (canada fork only): i18n support
@@ -114,12 +114,12 @@ def resource_validation_run(context, data_dict):
         raise t.ValidationError(
             {u'url': u'Resource must have a valid URL or an uploaded file'})
 
-    # (canada fork only): upload and white listed sources
+    # (canada fork only): upload and allowed domain sources
     allowed_domains = t.config.get('ckanext.canada.datastore_source_domain_allow_list', [])
     url = resource.get('url')
     url_parts = urlsplit(url)
     if (resource.get('url_type') != 'upload' and url_parts.netloc not in allowed_domains):
-        raise t.ValidationError({'url': 'Only uploaded resources and white listed sources can be validated.'})
+        raise t.ValidationError({'url': 'Only uploaded resources and allowed domain sources can be validated.'})
 
     # Check if there was an existing validation for the resource
 
